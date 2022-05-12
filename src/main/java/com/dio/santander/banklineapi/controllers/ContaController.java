6a39520bc8fabd8824ac2a7968cd676051b4ec2d
gabriel <jeronimogbariel@gmail.com>
2022-05-11 22:21:18 -0300

    CRUD correntista

--- src/main/java/com/dio/santander/banklineapi/controllers/ContaController.java
+++ src/main/java/com/dio/santander/banklineapi/controllers/ContaController.java
@@ -2,11 +2,8 @@ package com.dio.santander.banklineapi.controllers;
 
 import com.dio.santander.banklineapi.models.Conta;
 import com.dio.santander.banklineapi.repository.ContaRepository;
-import com.dio.santander.banklineapi.repository.CorrentistaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +17,11 @@ public class ContaController {
     @GetMapping
     public List<Conta> findAll() {
         return this.repository.findAll();
+    }
+
+    @PostMapping
+    public void save(@RequestBody Conta conta) {
+
     }
 }
 
---
+++ src/main/java/com/dio/santander/banklineapi/service/ContaServiceImplements.java
@@ -0,0 +1,18 @@
+package com.dio.santander.banklineapi.service;
+
+import com.dio.santander.banklineapi.models.Conta;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContaServiceImplements implements ContaService {
+
+    @Override
+    public Conta save(Conta conta) {
+        return null;
+    }
+
+    @Override
+    public Conta findIdCorrentista(long idConta) {
+        return null;
+    }
+}
